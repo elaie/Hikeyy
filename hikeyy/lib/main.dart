098cf473c1b793584ffe7d4d3fed7fa8b052d219
Fr1day111 <nwrsudeep@gmail.com>
2023-05-31 18:28:51 +0545

    AuthWOrk

--- hikeyy/lib/main.dart
+++ hikeyy/lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hikeyy/screens/login_signup/login.dart';
 import 'package:hikeyy/screens/login_signup/signup.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp().then((value){
+    print('********************');
+  });
   runApp(const MyApp());
 }
 
@@ -14,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: "Poppins"),
-      home: const Signup(),
+      home: const LoginPage(),
     );
   }
 }
