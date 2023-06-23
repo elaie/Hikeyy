681154a62f7d3c0ceb01c8cae0006285227f6e6a
pritisha123 <pritishashresthaa24@gmail.com>
2023-06-23 15:52:14 +0545

    plan trip page

---
+++ hikeyy/lib/widgets/app_colors.dart
@@ -0,0 +1,6 @@
+import 'dart:ui';
+
+class AppColor {
+  static const Color primaryColor = Color.fromARGB(255, 99, 164, 103);
+  static  Color primaryLightColor = const Color.fromARGB(255, 130, 200, 133).withOpacity(0.5);
+}
