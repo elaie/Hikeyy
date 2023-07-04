f3ae8ea9efc655913d04200b0815d0eef15b787c
Fr1day111 <nwrsudeep@gmail.com>
2023-07-04 19:09:34 +0545

    Little Fixes

--- hikeyy/lib/start_trail/friendslocation.dart
+++ hikeyy/lib/start_trail/friendslocation.dart
@@ -1,9 +1,14 @@
 import 'dart:async';
-
+import 'dart:typed_data';
+import 'dart:ui' as ui;
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+
+import '../screens/profile_page/widgets/profile_picture.dart';
+import '../screens/profile_page/widgets/user_name.dart';
 
 class LocationFriends extends StatefulWidget {
   final String id;
@@ -16,6 +21,12 @@ class LocationFriends extends StatefulWidget {
 class _LocationFriendsState extends State<LocationFriends> {
   Completer<GoogleMapController> _controller = Completer();
   List<Marker> _markers=[];
+  Future<Uint8List> getBytesFromAsset(String path, int width) async {
+    ByteData data = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(data.buffer.asUint8List(), targetWidth: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!.buffer.asUint8List();
+  }
   //late Position UserPositon;
   static late final CameraPosition _kGoogle;
   Future<void> getUserCurrentLocation() async {
@@ -32,16 +43,71 @@ class _LocationFriendsState extends State<LocationFriends> {
     });
   }
   Future<void> getLocations()async {
+    // BitmapDescriptor markerbitmap = await BitmapDescriptor.fromAssetImage(
+    //   ImageConfiguration(size: Size.square(1),
+    //   devicePixelRatio: 10),
+    //   "assets/images/profile.png",
+    // );
+    final Uint8List markerIcon = await getBytesFromAsset('assets/icons/profile.png', 60);
     QuerySnapshot datas = await FirebaseFirestore.instance.collection('Groups').doc(widget.id).collection('Locations').get();
     datas.docs.forEach((element) {
       GeoPoint point = element['Position'];
       String id = element.id;
-      //print(point.latitude );
-      //print(point.longitude);
-     // print('****************');
-      _markers.add(Marker(
+      _markers.add(
+        Marker(
         markerId: MarkerId(id),
         position: LatLng(point.latitude,point.longitude),
+        icon: BitmapDescriptor.fromBytes(markerIcon),
+          onTap: (){
+            showDialog(
+              context: context,
+              builder: (context) => FutureBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                future: FirebaseFirestore.instance
+                    .collection('Users')
+                    .doc(id)
+                    .get(),
+                builder: (_, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                    var data = snapshot.data!.data();
+                    var name = data!['UserName'];
+                    var pfp = data['pfpUrl'];
+                    return AlertDialog(
+                  content: Row(
+                    children: [
+                      Container(
+                        height: 40,
+                        width: 40,
+                        decoration: BoxDecoration(
+                            color: Colors
+                                .blueAccent,
+                            borderRadius:
+                            BorderRadius
+                                .circular(
+                                200),
+                            image: DecorationImage(
+                                fit:
+                                BoxFit.fill,
+                                image: NetworkImage(pfp))),
+                      ),
+                     SizedBox(width: 10,),
+                     Text(name)
+                    ],
+                  ),
+                  actions: <Widget>[
+                    TextButton(
+                      onPressed: () =>
+                          Navigator.of(context).pop(),
+                      child: new Text('Dismiss'),
+                    ),
+                  ],
+                );}
+              ),
+            );
+          }
       ),);
     }
     );
@@ -51,9 +117,9 @@ class _LocationFriendsState extends State<LocationFriends> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    getLocations();
+   // getLocations();
    // _markers=getLocations() as List<Marker>;
-     getUserCurrentLocation();
+    // getUserCurrentLocation();
   }
   @override
   Widget build(BuildContext context) {
